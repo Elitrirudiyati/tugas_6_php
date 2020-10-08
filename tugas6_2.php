0a05e25b1053ad54a3add681edaa5a48654a266b
Elitrirudiyati <71365611+Elitrirudiyati@users.noreply.github.com>
2020-10-08 10:24:00 +0700

    Add files via upload

---
+++ tugas6_2.php
@@ -0,0 +1,14 @@
+<?php
+$harga_jambu=15000;
+$harga_kardus=2000;
+$jumlah_dus=2;
+$total_jambu=6*5;
+$total_penjualan=($total_jambu*$harga_jambu)+($harga_kardus*$jumlah_dus);
+ echo "=================================================";
+ echo "<br>";
+ echo "Harga Jambu = Rp $harga_jambu / Kg <br>";
+ echo "Harga Kardus = Rp $harga_kardus / pcs <br>";
+ echo "Total Penjualan (Dus dan Jambu ) = Rp $total_penjualan <br>";
+ echo "================================================";
+
+?>
